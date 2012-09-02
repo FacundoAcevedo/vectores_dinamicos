5de3099dd2d0ad99c226668862b6d842f8d76046
Facundo Acevedo <acv2facundo@gmail.com>
2012-09-02 18:08:14 -0300

    agregada una opcion al runvalgrind.sh

--- runvalgrind.sh
+++ runvalgrind.sh
@@ -1,1 +1,1 @@
-valgrind --leak-check=yes --track-origins=yes --show-reachable=yes   ./vector_dinamico.bin
+valgrind --leak-resolution=med --leak-check=yes --track-origins=yes --show-reachable=yes   ./vector_dinamico.bin
